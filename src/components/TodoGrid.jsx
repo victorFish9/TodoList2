ce5f7487b2e8c5212df05867ab259432d4264d61
Victor <aue228lublupacanov@gmail.com>
2023-10-08 17:42:32 +0300

    App is done

--- src/components/TodoGrid.jsx
+++ src/components/TodoGrid.jsx
@@ -2,18 +2,26 @@ import { AgGridReact } from "ag-grid-react";
 import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
 import 'ag-grid-community/styles/ag-theme-material.css'; // Optional theme CSS
 import { useRef } from "react";
+import { AgGridColumn } from "ag-grid-react";
 
 export default function TodoGrid(props) {
 
     const gridRef = useRef();
+
+    const gridOptions = {
+        animateRows: true,
+    }
     console.log(props)
 
     const c = [{
-        headerName: "Description", field: "desctiption", sortable: true, filter: true
+        headerName: "Description", field: "desctiption", sortable: true, filter: true, floatingFilter: true,
     },
     {
-        headerName: "Date", field: "date", sortable: true, filter: true,
-        cellStyle: params => params.value > 3 ? { color: 'red' } : { color: 'green' }
+        headerName: "Date", field: "date", sortable: true, filter: true, floatingFilter: true,
+    },
+    {
+        headerName: "Priority", field: "priority", sortable: true, filter: true, floatingFilter: true,
+        cellStyle: params => params.value == "High" ? { color: 'red' } : { color: 'green' }
     }
     ]
 
@@ -33,7 +41,8 @@ export default function TodoGrid(props) {
             <button onClick={deleteSelect}>Delete</button>
             <div className="ag-theme-material" style={{ height: '700px', width: '70%', margin: 'auto' }}>
                 <AgGridReact rowData={props.todos} columnDefs={c}
-                    rowSelection="single" onGridReady={params => gridRef.current = params.api}>
+                    rowSelection="single" onGridReady={params => gridRef.current = params.api} gridOptions={gridOptions}>
+
 
                 </AgGridReact>
             </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function TodoList(props) {
 
-    const [desc, setDesc] = useState({ desctiption: "", date: "" });
+    const [desc, setDesc] = useState({ desctiption: "", date: "", priority: "" });
 
     const inputDescription = (event) => {
         setDesc({ ...desc, desctiption: event.target.value });
@@ -12,10 +12,14 @@ export default function TodoList(props) {
         setDesc({ ...desc, date: event.target.value });
     }
 
+    const inputPriority = (event) => {
+        setDesc({ ...desc, priority: event.target.value });
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         props.onSubmit(desc);
-        setDesc({ desctiption: "", date: "" })
+        setDesc({ desctiption: "", date: "", priority: "" })
     }
 
     return (
@@ -26,6 +30,8 @@ export default function TodoList(props) {
                 <input type='text' value={desc.desctiption} onChange={inputDescription} required name="Description" />
                 <label>Date:</label>
                 <input type='date' value={desc.date} onChange={inputDate} required name="Date" />
+                <label>Priority</label>
+                <input type="text" value={desc.priority} onChange={inputPriority} required name="Priority" />
                 <input type='submit' value="add" />
             </form>
         </div>
